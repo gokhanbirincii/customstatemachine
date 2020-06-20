ea2eb7c62d7a3a344b078604ff00b2556b328467
Gokhan Birinci <gokhan.birinci@trendyol.com>
2020-06-21 00:43:48 +0300

    polishing

--- src/main/java/com/trendyol/customstatemachine/state_machine/state/InitialState.java
+++ src/main/java/com/trendyol/customstatemachine/state_machine/state/InitialState.java
@@ -4,7 +4,6 @@ import com.trendyol.customstatemachine.annotation.Statable;
 import com.trendyol.customstatemachine.state_machine.StateMachineEvent;
 import com.trendyol.customstatemachine.state_machine.StateMachineState;
 import com.trendyol.customstatemachine.state_machine.model.TransitionModel;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.statemachine.action.Action;
 
@@ -26,12 +25,8 @@ public class InitialState extends State {
     @Override
     public List<TransitionModel> getTransitions() {
 
-        final var transitions = new ArrayList<TransitionModel>();
-
-        transitions.add(new TransitionModel(getSourceState(), StateMachineState.ORDER_RECEIVED,
+        return List.of(new TransitionModel(getSourceState(), StateMachineState.ORDER_RECEIVED,
                 StateMachineEvent.GET_ORDER_RECEIVED));
-
-        return transitions;
     }
 
     @Override
--- src/main/java/com/trendyol/customstatemachine/state_machine/state/OrderReceived.java
+++ src/main/java/com/trendyol/customstatemachine/state_machine/state/OrderReceived.java
@@ -4,7 +4,6 @@ import com.trendyol.customstatemachine.annotation.Statable;
 import com.trendyol.customstatemachine.state_machine.StateMachineEvent;
 import com.trendyol.customstatemachine.state_machine.StateMachineState;
 import com.trendyol.customstatemachine.state_machine.model.TransitionModel;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.statemachine.action.Action;
 
@@ -26,12 +25,8 @@ public class OrderReceived extends State {
     @Override
     public List<TransitionModel> getTransitions() {
 
-        final var transitions = new ArrayList<TransitionModel>();
-
-        transitions.add(new TransitionModel(getSourceState(), StateMachineState.SUPPLIED,
-                StateMachineEvent.GET_SUPPLIED));
-
-        return transitions;
+        return List.of(new TransitionModel(getSourceState(), StateMachineState.ORDER_RECEIVED,
+                StateMachineEvent.GET_ORDER_RECEIVED));
     }
 
     @Override
